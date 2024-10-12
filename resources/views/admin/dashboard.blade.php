7ad3ac87a4049e7eb342f30eeb11a4154c9b4746
Grant Brivael <granttiwa0@gmail.com>
2024-10-12 11:32:27 +0100

    rollback sur mysql

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -151,7 +151,7 @@
                                         <div class="icon">
                                             <i class="ion ion-card"></i>
                                         </div>
-                                        <a href="" class="small-box-footer"> <i class="fas fa-arrow-circle-right"></i></a>
+                                        <a href="{{ route('order.create') }}" class="small-box-footer"> <i class="fas fa-arrow-circle-right"></i></a>
                                     </div>
                                 </div>
                                 <div class="col-lg-2 col-4">
