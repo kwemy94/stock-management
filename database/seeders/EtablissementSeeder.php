89950b4441e94d232811036bf49cc310c06b6249
Grant Brivael <granttiwa0@gmail.com>
2024-11-02 12:01:19 +0100

    mig front on

--- database/seeders/EtablissementSeeder.php
+++ database/seeders/EtablissementSeeder.php
@@ -16,7 +16,7 @@ class EtablissementSeeder extends Seeder
         $etablissements = array(
             array(
                 'name' => "app-aministration-tech-briva",
-                'email' => 'tibriva2302@gmail.com',
+                'email' => 'tigod2302@gmail.com',
                 'status' => 1,
                 'settings' => json_encode(array("db" => array('database' => 'audien6989_db_gt2', 'username' => 'audien6989_gt2', 'password' => 'P4?2D7dv2rnf'), 'momo' => '{}')),
             )
